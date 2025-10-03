4cfafd231b60cfcc25d20b26e55a30ca0b8f63ef
Fang-Pen Lin <fangpen@infisical.com>
2025-10-02 19:27:05 -0700

    Make deps optional, revert unwanted changes

--- src/main/java/com/infisical/sdk/resources/AuthClient.java
+++ src/main/java/com/infisical/sdk/resources/AuthClient.java
@@ -1,12 +1,12 @@
 package com.infisical.sdk.resources;
 
-import com.infisical.sdk.api.ApiClient;
-import com.infisical.sdk.models.AwsAuthLoginInput;
 import com.infisical.sdk.models.LdapAuthLoginInput;
 import com.infisical.sdk.models.MachineIdentityCredential;
-import com.infisical.sdk.models.UniversalAuthLoginInput;
 import com.infisical.sdk.util.InfisicalException;
 import java.util.function.Consumer;
+import com.infisical.sdk.api.ApiClient;
+
+import com.infisical.sdk.models.UniversalAuthLoginInput;
 
 public class AuthClient {
   private final ApiClient apiClient;
@@ -18,37 +18,26 @@ public class AuthClient {
   }
 
   public void UniversalAuthLogin(String clientId, String clientSecret) throws InfisicalException {
-    var params =
-        UniversalAuthLoginInput.builder().clientId(clientId).clientSecret(clientSecret).build();
+      var params = UniversalAuthLoginInput.builder()
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .build();
 
-    var url =
-        String.format("%s%s", this.apiClient.GetBaseUrl(), "/api/v1/auth/universal-auth/login");
-    var credential = this.apiClient.post(url, params, MachineIdentityCredential.class);
-    this.onAuthenticate.accept(credential.getAccessToken());
+      var url = String.format("%s%s", this.apiClient.GetBaseUrl(), "/api/v1/auth/universal-auth/login");
+      var credential = this.apiClient.post(url, params, MachineIdentityCredential.class);
+      this.onAuthenticate.accept(credential.getAccessToken());
   }
 
   public void LdapAuthLogin(LdapAuthLoginInput input) throws InfisicalException {
-    var validationMsg = input.validate();
+      var validationMsg = input.validate();
 
-    if (validationMsg != null) {
-      throw new InfisicalException(validationMsg);
-    }
+      if (validationMsg != null) {
+          throw  new InfisicalException(validationMsg);
+      }
 
-    var url = String.format("%s%s", this.apiClient.GetBaseUrl(), "/api/v1/auth/ldap-auth/login");
-    var credential = this.apiClient.post(url, input, MachineIdentityCredential.class);
-    this.onAuthenticate.accept(credential.getAccessToken());
-  }
-
-  public void AwsAuthLogin(AwsAuthLoginInput input) throws InfisicalException {
-    var validationMsg = input.validate();
-
-    if (validationMsg != null) {
-      throw new InfisicalException(validationMsg);
-    }
-
-    var url = String.format("%s%s", this.apiClient.GetBaseUrl(), "/api/v1/auth/aws-auth/login");
-    var credential = this.apiClient.post(url, input, MachineIdentityCredential.class);
-    this.onAuthenticate.accept(credential.getAccessToken());
+      var url = String.format("%s%s", this.apiClient.GetBaseUrl(), "/api/v1/auth/ldap-auth/login");
+      var credential = this.apiClient.post(url, input, MachineIdentityCredential.class);
+      this.onAuthenticate.accept(credential.getAccessToken());
   }
 
   public void SetAccessToken(String accessToken) {
