cb1c340e051b8ea97a58924527d49993237c94e0
Fang-Pen Lin <fangpen@infisical.com>
2025-10-02 18:11:20 -0700

    Use v2 aws sdk instead

--- src/main/java/com/infisical/sdk/auth/AwsAuthProvider.java
+++ src/main/java/com/infisical/sdk/auth/AwsAuthProvider.java
@@ -1,34 +1,40 @@
 package com.infisical.sdk.auth;
 
-import com.amazonaws.DefaultRequest;
-import com.amazonaws.Request;
-import com.amazonaws.auth.AWS4Signer;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.http.HttpMethodName;
-import com.amazonaws.util.SdkHttpUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infisical.sdk.models.AwsAuthLoginInput;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.http.ContentStreamProvider;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.http.SdkHttpRequest;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4FamilyHttpSigner;
+import software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner;
 
 @Data
 @Builder
 public class AwsAuthProvider {
   private static final ObjectMapper objectMapper = new ObjectMapper();
 
-  @Builder.Default private final String serviceName = "sts";
-  @Builder.Default private final HttpMethodName methodName = HttpMethodName.POST;
-  @Builder.Default private final String endpointTemplate = "https://sts.%s.amazonaws.com";
+  @NonNull @Builder.Default private final String serviceName = "sts";
+  @NonNull @Builder.Default private final SdkHttpMethod httpMethod = SdkHttpMethod.POST;
+  @NonNull @Builder.Default private final String endpointTemplate = "https://sts.%s.amazonaws.com";
 
-  @Builder.Default
+  @NonNull @Builder.Default
+  private final String contentType = "application/x-www-form-urlencoded; charset=utf-8";
+
+  @NonNull @Builder.Default
   private final Map<String, List<String>> params =
       Map.ofEntries(
           Map.entry("Action", List.of("GetCallerIdentity")),
@@ -36,32 +42,38 @@ public class AwsAuthProvider {
 
   private final Date overrideDate;
 
-  public AwsAuthLoginInput fromCredentials(String region, AWSCredentials credentials)
-      throws URISyntaxException {
+  public AwsAuthLoginInput fromCredentials(String region, AwsCredentials credentials) {
 
-    final Request<?> request = new DefaultRequest<>(serviceName);
+    final AwsV4HttpSigner signer = AwsV4HttpSigner.create();
+
     final String iamRequestURL = endpointTemplate.formatted(region);
-    request.setHttpMethod(methodName);
-    request.setEndpoint(new URI(iamRequestURL));
-    request.setParameters(params);
-    final String iamRequestBody = SdkHttpUtils.encodeParameters(request);
+    final String iamRequestBody = encodeParameters(params);
+    final SdkHttpFullRequest request =
+        SdkHttpFullRequest.builder()
+            .uri(URI.create(iamRequestURL))
+            .method(httpMethod)
+            .appendHeader("Content-Type", contentType)
+            .appendHeader("Content-Length", String.valueOf(iamRequestBody.length()))
+            .build();
 
-    final AWS4Signer signer = new AWS4Signer();
-    signer.setServiceName(serviceName);
-    signer.setRegionName(region);
-    if (overrideDate != null) {
-      signer.setOverrideDate(overrideDate);
-    }
-    signer.sign(request, credentials);
+    final SdkHttpRequest signedRequest =
+        signer
+            .sign(
+                signingRequest ->
+                    signingRequest
+                        .request(request)
+                        .identity(credentials)
+                        .payload(
+                            ContentStreamProvider.fromByteArray(
+                                iamRequestBody.getBytes(StandardCharsets.UTF_8)))
+                        .putProperty(AwsV4FamilyHttpSigner.SERVICE_SIGNING_NAME, serviceName)
+                        .putProperty(AwsV4HttpSigner.REGION_NAME, region))
+            .request();
 
-    final Map<String, String> requestHeaders = request.getHeaders();
-    // TODO: add content-type and content length?
-    //       ref:
-    // https://github.com/Infisical/go-sdk/blob/3f1b7f831a883ea30a9182f86bd0173f4cf82a22/auth.go#L293-L294
-
+    final Map<String, List<String>> requestHeaders = signedRequest.headers();
     try {
       return AwsAuthLoginInput.builder()
-          .iamHttpRequestMethod(methodName.name())
+          .iamHttpRequestMethod(httpMethod.name())
           .iamRequestHeaders(
               Base64.getEncoder()
                   .encodeToString(
@@ -74,5 +86,17 @@ public class AwsAuthProvider {
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public static String encodeParameters(Map<String, List<String>> params) {
+    return params.entrySet().stream()
+        .flatMap(entry -> entry.getValue().stream().map(item -> Map.entry(entry.getKey(), item)))
+        .map(
+            entry ->
+                String.format(
+                    "%s=%s",
+                    URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8),
+                    URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8)))
+        .collect(Collectors.joining("&"));
   }
 }
